b5d1095951bc2b65d1c48a75d6f4042211f0d967
christian175 <55259741+christian175@users.noreply.github.com>
2021-02-19 22:34:42 -0500

    add readme

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 class Main{
     public static void main(String[] args) {
         Book book = new Book("J.K. Rowling", 500);
-        System.out.println(book);
+        System.out.println("hello");
     }
 }
